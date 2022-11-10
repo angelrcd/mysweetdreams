b18e39a8a9a7f5357f6767c95e37f5e4a35cd8f2
Angel Romero <mrangelrcd@gmail.com>
2022-11-10 12:50:23 +0100

    Description component

---
+++ src/components/Description.jsx
@@ -0,0 +1,41 @@
+import './MainPage.css'
+import ButtonComenzar from './ButtonComenzar.jsx'
+
+function Description () {
+  return (
+    <div id="Description">
+    <div id="info" className="h-auto bg-web-fondo flex flex-col justify-center">
+  <div className="px-56">
+    <h3 className="border-b-4 border-black py-16 px-44 text-3xl text-center">Una experiencia personalizada para que duermas
+      mejor
+      durante la
+      noche y
+      estes mas
+      despierto durante el día</h3>
+  </div>
+  <div className="py-16 flex justify-around flex-wrap">
+    <div className="w-36 flex flex-col gap-4 mx-20 mb-10">
+      <img className="h-20" src="/icons/monitor-tv-svgrepo-com.svg" alt="" />
+      <h4 className="text-2xl text-center">Experiencia personalizada</h4>
+      <p className="text-center text-gray-600">Nos adaptamos a cada usuario para proporcionar una informacion unica</p>
+    </div>
+
+    <div className="w-36 flex flex-col gap-4 mx-20 mb-10">
+      <img className="h-20" src="/icons/world-svgrepo-com.svg" alt="" />
+      <h4 className="text-2xl text-center">Todo en uno</h4>
+      <p className="text-center text-gray-600">Desde nuestra aplicación podras introducir, gestionar y visualizar todos tus
+        datos y recomendaciones</p>
+    </div>
+    <div className="w-36 flex flex-col gap-4 mx-20 mb-10">
+      <img className="h-20" src="/icons/shield-svgrepo-com.svg" alt="" />
+      <h4 className="text-2xl text-center">Segura y eficaz</h4>
+      <p className="text-center text-gray-600">Tus datos estan protegidos frente a terceros</p>
+    </div>
+  </div>
+  <ButtonComenzar />
+</div>
+    </div>
+  )
+}
+
+export default Description
--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,13 +1,16 @@
 import './MainPage.css'
 import ButtonComenzar from './ButtonComenzar.jsx'
+import Description from './Description.jsx'
 
 function MainPage () {
   return (
-  <div className="bg-fixed w-screen h-screen bg-cover bg-[url('/img/background.jpg')] flex flex-col justify-around items-center" >
-    <h1 className="text-white text-6xl">SWEETDREAMS</h1>
-    <h2 className="text-white text-5xl">Duerme mejor. Siéntete mejor</h2>
+  <div className="bg-fixed w-screen h-screen bg-cover bg-[url('/img/background-night.jpg')] flex flex-col justify-around items-center" >
+    <h1 className="text-white font-bluetea text-6xl">SWEETDREAMS</h1>
+    <h2 className="text-white font-bluetea text-5xl">Duerme mejor. Siéntete mejor</h2>
     <ButtonComenzar />
   </div>
+  <Description />
+
   )
 }
 
