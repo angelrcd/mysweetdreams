8f428654271f96cef84fe16597054786c7641ea5
Daniel Tallón <daniel.tallon.ortega@gmail.com>
2022-11-10 16:28:43 +0100

    fix typos and page title icon

--- src/components/ContentLogin.jsx
+++ src/components/ContentLogin.jsx
@@ -6,7 +6,7 @@ function ContentLogin() {
         <div className="bg-[url('./img/background.jpg')] h-screen w-full bg-cover flex flex-col justify-start items-center">
             <h2 className="font-bluetea text-8xl text-black mb-16 mt-28">SWEETDREAMS</h2>
             <FormLogin />
-            <div className="bg-blue-800 rounded-2xl py-1 px-6 bg-opacity-50 my-2 text-opacity-100"><p className="text-white">Forgot password? | Don’t have an acount yet? <a href="" className="underline">Sign up</a></p></div>
+            <div className="bg-blue-800 rounded-2xl py-1 px-6 bg-opacity-50 my-2 text-opacity-100"><p className="text-white">Forgot password? | Don’t have an account yet? <a href="" className="underline">Sign up</a></p></div>
         </div>
     );
 }
