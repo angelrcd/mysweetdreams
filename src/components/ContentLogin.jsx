5565e89248348a16690f509847e41d07a69d75e4
Daniel Tallón <daniel.tallon.ortega@gmail.com>
2022-11-10 14:45:49 +0100

    fix some small typos

--- src/components/ContentLogin.jsx
+++ src/components/ContentLogin.jsx
@@ -1,13 +1,13 @@
-import "./ContentLogin.css";
-import FormLogin from "./FormLogin.jsx";
+import './ContentLogin.css';
+import FormLogin from './FormLogin.jsx';
 
-function ContentLogin (){
+function ContentLogin() {
     return (
-    <div className="bg-[url('./img/background.jpg')] h-screen w-full bg-cover flex flex-col justify-start items-center">
-        <h2 className="font-bluetea text-8xl text-black mb-16 mt-28">SWEETDREAMS</h2>
-        <FormLogin />
-        <div className="bg-blue-800 rounded-2xl py-1 px-6 bg-opacity-50 my-2 text-opacity-100"><p class="text-white">Forgot password? | Don’t have an acoount yet? <a href="" className="underline">Sign up</a></p></div>
-    </div>
+        <div className="bg-[url('./img/background.jpg')] h-screen w-full bg-cover flex flex-col justify-start items-center">
+            <h2 className="font-bluetea text-8xl text-black mb-16 mt-28">SWEETDREAMS</h2>
+            <FormLogin />
+            <div className="bg-blue-800 rounded-2xl py-1 px-6 bg-opacity-50 my-2 text-opacity-100"><p className="text-white">Forgot password? | Don’t have an acount yet? <a href="" className="underline">Sign up</a></p></div>
+        </div>
     );
 }
 
