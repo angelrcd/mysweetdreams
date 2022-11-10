9b1270cb18088d3e243c786a003f386ac0fa1180
Angel Romero <mrangelrcd@gmail.com>
2022-11-10 16:24:39 +0100

    Fixed width

--- src/components/LandingImage.jsx
+++ src/components/LandingImage.jsx
@@ -3,7 +3,7 @@ import ButtonComenzar from './ButtonComenzar.jsx'
 
 function LandingImage () {
   return (
-    <div className="bg-fixed w-screen h-screen bg-cover bg-[url('/img/background-night.jpg')] flex flex-col justify-around items-center" >
+    <div className="bg-fixed h-screen bg-cover bg-[url('/img/background-night.jpg')] flex flex-col justify-around items-center" >
       <h1 className="text-white font-bluetea text-6xl">SWEETDREAMS</h1>
       <h2 className="text-white font-bluetea text-5xl">Duerme mejor. Siéntete mejor</h2>
       <ButtonComenzar />
