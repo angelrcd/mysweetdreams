635920691f3000778f65bc7ce960f78a46aa1b18
Angel Romero <mrangelrcd@gmail.com>
2022-11-10 11:50:19 +0100

    button component

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,11 +1,12 @@
 import './MainPage.css'
+import ButtonComenzar from './ButtonComenzar.jsx'
 
 function MainPage () {
   return (
-  <div className="bg-fixed w-screen h-screen bg-cover flex flex-col justify-around items-center">
-    <h1 className="text-black text-6xl">SWEETDREAMS</h1>
-    <h2 className="text-black text-5xl">Duerme mejor. Siéntete mejor</h2>
-    <button className="boton-web mb-10">COMENZAR YA</button>
+  <div className="bg-fixed w-screen h-screen bg-cover bg-[url('/img/background.jpg')] flex flex-col justify-around items-center" >
+    <h1 className="text-white text-6xl">SWEETDREAMS</h1>
+    <h2 className="text-white text-5xl">Duerme mejor. Siéntete mejor</h2>
+    <ButtonComenzar />
   </div>
   )
 }
