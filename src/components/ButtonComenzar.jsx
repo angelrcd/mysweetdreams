5a24cea42816e46fa4d21611fc687de738729e11
Eduardo Diez <edudm95@gmail.com>
2022-11-11 13:11:56 +0100

    Added new style to login page
    > New background
    > Using new button

--- src/components/ButtonComenzar.jsx
+++ src/components/ButtonComenzar.jsx
@@ -1,9 +1,9 @@
 import './MainPage.css'
 
-function ButtonComenzar () {
+function ButtonComenzar (props) {
   return (
     <button className="font-quicksand font-bold w-fit px-16 h-20 text-3xl bg-web-boton hover:bg-web-boton2 hover:text-white rounded-full mb-10 shadow-lg duration-500 ease-in-out hover:resize hover:ring-2 ring-offset-4 hover:scale-105 ring-web-boton" >
-      <span>COMENZAR YA</span>
+      <span>{props.text}</span>
     </button>
   )
 }
