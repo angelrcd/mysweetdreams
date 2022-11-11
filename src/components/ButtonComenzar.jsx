1d03a7f5928bdd990e0afc473fdacec1e00ae21a
Angel Romero <mrangelrcd@gmail.com>
2022-11-11 11:43:30 +0100

    Added hover effect on button component

--- src/components/ButtonComenzar.jsx
+++ src/components/ButtonComenzar.jsx
@@ -2,7 +2,7 @@ import './MainPage.css'
 
 function ButtonComenzar () {
   return (
-    <button className="font-quicksand w-1/4 h-28 text-5xl bg-web-boton rounded-full mb-10 shadow-lg">COMENZAR YA</button>
+    <button className="font-quicksand font-bold w-fit px-16 h-20 text-3xl bg-[#79b5f3] hover:bg-[#3f67bc] hover:text-white rounded-full mb-10 shadow-lg duration-500 ease-in-out hover:resize hover:ring-2 ring-offset-4 hover:scale-105 ring-web-boton" >COMENZAR YA</button>
   )
 }
 
--- src/components/LandingImage.jsx
+++ src/components/LandingImage.jsx
@@ -4,8 +4,8 @@ import ButtonComenzar from './ButtonComenzar.jsx'
 function LandingImage () {
   return (
     <div className="bg-fixed h-screen bg-cover bg-[url('/img/background-night.jpg')] flex flex-col justify-around items-center" >
-      <h1 className="text-white font-bluetea text-6xl">SWEETDREAMS</h1>
-      <h2 className="text-white font-caveat text-5xl">Duerme mejor. Siéntete mejor</h2>
+      <h1 className="text-white font-bluetea text-7xl">SWEETDREAMS</h1>
+      <h2 className="subtitulo text-white font-caveat text-5xl">Duerme mejor. Siéntete mejor</h2>
       <ButtonComenzar />
     </div>
   )
