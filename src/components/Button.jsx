9556c0e661d0fb5b56fa791dcbd048cd323cb29e
Angel Romero <mrangelrcd@gmail.com>
2022-11-15 11:14:26 +0100

    ready to merge

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,12 +1,13 @@
 import './MainPage.css'
+import { Link } from 'react-router-dom'
 
 function Button (props) {
   return (
-    <button className="font-quicksand font-bold w-fit px-10 py-2 text-3xl bg-web-boton hover:bg-web-boton2 hover:text-white rounded-full mb-10 shadow-lg duration-500 ease-in-out hover:resize hover:ring-2 ring-offset-4 hover:scale-105 ring-web-boton" >
-      <a href={props.to}>
-      <span>{props.text}</span>
-      </a>
-    </button>
+    <Link to={props.to}>
+      <button className="font-quicksand font-bold w-fit px-10 py-2 text-3xl bg-web-boton hover:bg-web-boton2 hover:text-white rounded-full mb-10 shadow-lg duration-500 ease-in-out hover:resize hover:ring-2 ring-offset-4 hover:scale-105 ring-web-boton" >
+        <span>{props.text}</span>
+      </button>
+    </Link>
   )
 }
 
