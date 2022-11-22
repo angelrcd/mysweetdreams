232d2b8e0bb37b2b6bad73361588121ae3c2a590
Angel Romero <mrangelrcd@gmail.com>
2022-11-22 16:10:27 +0100

    Added API call to test server port 3000

--- src/views/UserDataForm.jsx
+++ src/views/UserDataForm.jsx
@@ -8,14 +8,14 @@ function UserDataForm () {
         <h1 className="text-shadow-2 text-white font-bluetea text-6xl">SWEETDREAMS</h1>
       </div>
       <h2 className="text-shadow-1 text-white font-quicksand text-3xl font-bold text-center my-6">Gracias por registrarte, por favor rellena tus datos para que podamos darte un información mas personalizada</h2>
-      <form className='w-fit bg-gray-50 rounded-[40px] border-2 border-black pt-11 px-16 font-quicksand text-2xl flex flex-col gap-4' method="post" action="direccion api backend">
+      <form className='w-fit bg-gray-50 rounded-[40px] border-2 border-black pt-11 px-16 font-quicksand text-2xl flex flex-col gap-4' method="post" action="http://localhost:3000/test/">
         <fieldset className='flex flex-col gap-4'>
-        <label htmlFor="first-name">Nombre<input className='bg-gray-200 w-full rounded-md focus:outline-web-boton focus:shadow-outline py-2 px-5' id="first-name" name="first-name" type="text" placeholder="Introduce tu nombre" required /></label>
-        <label htmlFor="last-name">Apellidos<input className='bg-gray-200 w-full rounded-md focus:outline-web-boton focus:shadow-outline py-2 px-5' id="last-name" name="last-name" type="text" placeholder="Introduce tus apellidos" required /></label>
+        <label htmlFor="name">Nombre<input className='bg-gray-200 w-full rounded-md focus:outline-web-boton focus:shadow-outline py-2 px-5' id="name" name="name" type="text" placeholder="Introduce tu nombre" required /></label>
+        <label htmlFor="lastname">Apellidos<input className='bg-gray-200 w-full rounded-md focus:outline-web-boton focus:shadow-outline py-2 px-5' id="lastname" name="lastname" type="text" placeholder="Introduce tus apellidos" required /></label>
         <label htmlFor="birthday">Fecha de nacimiento<input className='bg-gray-200 w-full rounded-md focus:outline-web-boton focus:shadow-outline py-2 px-5' id="birthday" name="birthday" type="date" required /></label>
         </fieldset>
         <fieldset>
-          <label htmlFor="profile-picture">Foto de perfil<input className='bg-gray-200 w-full rounded-md focus:outline-web-boton focus:shadow-outline py-2 px-5' id="profile-picture" type="file" name="file" /></label>
+          <label htmlFor="profilepicture">Foto de perfil<input className='bg-gray-200 w-full rounded-md focus:outline-web-boton focus:shadow-outline py-2 px-5' id="profilepicture" type="file" name="file" /></label>
         </fieldset>
         <input className="cursor-pointer mx-auto mt-11 font-quicksand font-bold w-fit px-6 py-2 text-3xl bg-web-boton hover:bg-web-boton2 hover:text-white rounded-full mb-10 shadow-lg duration-500 ease-in-out hover:resize hover:ring-2 ring-offset-4 hover:scale-105 ring-web-boton" type="submit" value="Enviar y continuar" />
       </form>
