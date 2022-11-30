ba721f5a0922cb49d35b17247618ae9c0dc83a68
Angel Romero <mrangelrcd@gmail.com>
2022-11-30 16:53:00 +0100

    Mobile menu design changed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,7 @@ function Navbar () {
     <div className="flex justify-between items-center md:h-20 h-10 mx-auto px-4 text-black md:bg-transparent bg-white dark:bg-web-formBgDarkMode">
       <Link to='/'><div className='flex items-center gap-5 ml-4'>
         <img className='h-[40px] md:h-[70px]' src="/img/logo.png" alt="logo" />
-        <h1 className="hidden min-[400px]:block md:hidden xl:block md:text-3xl w-full text-shadow-1 lg:text-shadow-2 text-white font-bluetea">MYSWEETDREAMS</h1>
+        <h1 className="hidden md:hidden xl:block md:text-3xl w-full text-shadow-1 lg:text-shadow-2 text-white font-bluetea">MYSWEETDREAMS</h1>
       </div></Link>
       <ul className='hidden md:flex text-2xl text-shadow-1 font-semibold text-white'>
         <li className='pr-3 py-3 pl-0 hover:text-web-boton duration-300 ease-in-out'><Link to='/Login'>Iniciar sesión</Link></li>
@@ -21,17 +21,23 @@ function Navbar () {
         <li className='p-3 hover:text-web-boton duration-300 ease-in-out'><Link to='/Who'>Quienes somos</Link></li>
       </ul>
       <div onClick={handleNav} className='block md:hidden'>
-        {!nav ? <img className='h-[30px] filter dark:invert' src="/icons/cancel.svg" alt=" close menu button" /> : <img className='h-[30px] filter dark:invert' src="/icons/menu.svg" alt="menu button" />}
+        <img className='h-[30px] filter dark:invert' src="/icons/menu.svg" alt="menu button" />
       </div>
-      <div className={!nav ? 'fixed left-0 top-0 w-[60%] sm:w-[40%] h-fit border-r border-b border-gray-700 dark:border-gray-200 rounded-br-xl bg-web-fondo dark:bg-web-formBgDarkMode ease-in-out duration-500 z-10' : 'fixed left-[-100%] top-0 ease-in-out duration-500 z-10'}>
-
-        <h1 className='md:hidden w-full text-lg font-semibold font-bluetea text-web-boton text-shadow-1 mx-1'>SWEETDREAMS</h1>
-
+      <div className={!nav ? 'fixed right-0 top-0 w-[300px] h-screen flex flex-col  border-gray-700 dark:border-gray-200 bg-web-fondo dark:bg-web-formBgDarkMode ease-in-out duration-500 z-10' : 'fixed right-[-100%] top-0 w-[300px] h-screen flex flex-col ease-in-out duration-500 z-10'}>
+        <div className='flex justify-between'>
+          <p className='mt-3 ml-5 dark:text-white font-bluetea'>MYSWEETDREAMS</p>
+          <img onClick={handleNav} className='h-5 mr-3 mt-3 filter dark:invert' src="/icons/cancel.svg" alt="close menu button" />
+        </div>
         <ul className='p-4 dark:text-gray-100'>
           <li className='p-2 border-b border-gray-700 dark:border-gray-100'><Link to='/Login'>Iniciar sesión</Link></li>
           <li className='p-2 border-b border-gray-700 dark:border-gray-100'><Link to='/Signup'>Registrate</Link></li>
-          <li className='py-2'><Link to='/Who'>Quienes somos</Link></li>
+          <li className='p-2 border-b border-gray-700 dark:border-gray-100'><Link to='/Who'>Quienes somos</Link></li>
         </ul>
+        <div className="flex gap-5 px-6">
+          <img className="filter dark:invert h-6 cursor-pointer hover:scale-105 duration-500 ease-in-out" src="/icons/instagram.svg" alt="" />
+          <img className="filter dark:invert h-6 cursor-pointer hover:scale-105 duration-500 ease-in-out" src="/icons/twitter.svg" alt="" />
+          <img className="filter dark:invert h-6 cursor-pointer hover:scale-105 duration-500 ease-in-out" src="/icons/facebook.svg" alt="" />
+        </div>
       </div>
     </div>
   )
