b9832e43b0b487b6c371946aa8c03915cea4fb8b
Angel Romero <mrangelrcd@gmail.com>
2022-12-14 23:53:25 +0100

    added scroll module to app navbar

--- src/components/AppNavBar.jsx
+++ src/components/AppNavBar.jsx
@@ -2,6 +2,7 @@ import '../css/MainPage.css'
 import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { API, SOCIAL } from '../../data'
+import { enableScroll, disableScroll } from '../modules/disableEnableScroll'
 
 function AppNavbar () {
   // Detecta si el menu de movil esta abierto
@@ -77,46 +78,6 @@ function AppNavbar () {
       enableScroll()
     }
   }, [nav])
-
-  // left: 37, up: 38, right: 39, down: 40,
-  // spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
-  const keys = { 37: 1, 38: 1, 39: 1, 40: 1 }
-
-  function preventDefault (e) {
-    e.preventDefault()
-  }
-
-  function preventDefaultForScrollKeys (e) {
-    if (keys[e.keyCode]) {
-      preventDefault(e)
-      return false
-    }
-  }
-
-  // modern Chrome requires { passive: false } when adding event
-  let supportsPassive = false
-  try {
-    window.addEventListener('test', null, Object.defineProperty({}, 'passive', {
-      get: function () { supportsPassive = true }
-    }))
-  } catch (e) {}
-
-  const wheelOpt = supportsPassive ? { passive: false } : false
-  const wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel'
-
-  function disableScroll () {
-    window.addEventListener('DOMMouseScroll', preventDefault, false) // older FF
-    window.addEventListener(wheelEvent, preventDefault, wheelOpt) // modern desktop
-    window.addEventListener('touchmove', preventDefault, wheelOpt) // mobile
-    window.addEventListener('keydown', preventDefaultForScrollKeys, false)
-  }
-
-  function enableScroll () {
-    window.removeEventListener('DOMMouseScroll', preventDefault, false)
-    window.removeEventListener(wheelEvent, preventDefault, wheelOpt)
-    window.removeEventListener('touchmove', preventDefault, wheelOpt)
-    window.removeEventListener('keydown', preventDefaultForScrollKeys, false)
-  }
 
   function setYTo0 () {
     window.scrollTo(0, 0)
