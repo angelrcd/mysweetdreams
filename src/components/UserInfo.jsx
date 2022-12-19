fbd52f108912308ab9d69a21b35c1cf76a9baace
Angel Romero <mrangelrcd@gmail.com>
2022-12-19 17:50:59 +0100

    profile pic upload feature

--- src/components/UserInfo.jsx
+++ src/components/UserInfo.jsx
@@ -2,12 +2,16 @@ import React, { useState, useEffect, useRef } from 'react'
 import { useGetUserData } from '../modules/useGetUserData'
 import UploadPicButton from './UploadPicButton'
 import { Oval } from 'react-loader-spinner'
-import { changeDBUserProfilePic } from '../modules/fetchUserProfilePicture'
+import { changeDBUserProfilePic, uploadPicToServer } from '../modules/changeUserProfilePicture'
 import { logout } from '../modules/logout'
 
 function UserInfo () {
-  const { userData, isLoading, error } = useGetUserData()
-  const [image, setImage] = useState(false)
+  const handleImage = (pfp) => {
+    setImage('https://img.mysweetdreams.es/img/pfp/' + pfp)
+  }
+
+  const { userData, isLoading, error } = useGetUserData(handleImage)
+  const [image, setImage] = useState('https://img.mysweetdreams.es/img/pfp/' + userData.profilePic)
   const fileInput = useRef()
   const selectFile = () => {
     fileInput.current.click()
@@ -17,15 +21,14 @@ function UserInfo () {
     console.log(userData.profilePic)
     if (event.target.files && event.target.files[0]) {
       changeDBUserProfilePic(userData._id)
-      setTimeout(setImage(!image), 2000)
-      console.log(URL.createObjectURL(event.target.files[0]))
-      console.log(image)
+      setImage(URL.createObjectURL(event.target.files[0]))
+      uploadPicToServer(event.target.files[0], userData._id)
     }
   }
 
   const profileUserInfo = (
     <div className='w-full h-[300px] flex flex-col justify-center items-center p-2'>
-      <img title="Foto de perfil de usuario" src={'https://img.mysweetdreams.es/img/pfp/' + userData.profilePic} alt="user profile picture" className='rounded-full h-[220px] w-[220px] border-2 border-black object-cover' />
+      <img title="Foto de perfil de usuario" src={image} alt="user profile picture" className='rounded-full h-[220px] w-[220px] border-2 border-black object-cover' />
       <input className='hidden' type="file" ref={fileInput} onChange={onImageChange} />
       <span onClick={selectFile}>
         <UploadPicButton />
---
+++ src/modules/changeUserProfilePicture.js
@@ -0,0 +1,29 @@
+import { API, IMAGES_URL } from '../../data'
+
+export function changeDBUserProfilePic (userId) {
+  const options = {
+    credentials: 'include',
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ profilePic: userId + '.png' })
+  }
+
+  fetch(API.USERS.MY_USER, options)
+}
+
+export function uploadPicToServer (picture, userId) {
+  const blob = picture.slice(0, picture.size, 'image/png')
+  const newFile = new File([blob], userId + '.png', { type: 'image/png' })
+  const options = {
+    credentials: 'include',
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: picture
+  }
+  console.log(newFile)
+  fetch(IMAGES_URL, newFile)
+}
