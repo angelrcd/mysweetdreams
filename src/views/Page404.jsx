0ab2eac6f934dfd305fd1f026ce9bfc676ea8569
Daniel Tallón <daniel.tallon.ortega@gmail.com>
2022-12-20 14:01:45 +0100

    add error 404 page to router

---
+++ src/views/Page404.jsx
@@ -0,0 +1,9 @@
+function Page404() {
+    return (<div className="container">
+        <h1>Error 404</h1>
+        <p>¡No puedes encontrar la página que estás buscando! ¿Es posible que te hayas quedado dormido y hayas soñado con ella?</p>
+        <a href="/" className="btn">Volver al inicio</a>
+    </div>)
+};
+
+export default Page404
