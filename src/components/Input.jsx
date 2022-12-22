7ad25d0fa5872808081334d2f623536ac94c065f
Angel Romero <mrangelrcd@gmail.com>
2022-12-22 15:33:25 +0100

    Fixed inputs and button hovers not showing correctlu

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -5,7 +5,7 @@ function Input (props) {
     <label htmlFor={props.id}>{props.header}
       <input id={props.id} name={props.id} type={props.type} placeholder={props.placeholder} pattern={props.pattern}
       title={props.title} required min={props.min} max={props.max} value={props.value} onInput={props.onInput} maxLength={props.maxlength} minLength={props.minlength}
-      className='bg-gray-200 dark:bg-[#0a0a23] w-full rounded-md focus:shadow-outline py-2 px-5' />
+      className='form bg-gray-200 dark:bg-[#0a0a23] w-full rounded-md focus:shadow-outline py-2 px-5' />
     </label>
   )
 }
